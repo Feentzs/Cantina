3f55178daa52c044f86d9c738ce316267981e4cc
Feentzs <theshoyo304@gmail.com>
2025-05-24 14:59:15 -0300

    iniciando a tela do balcão

---
+++ telabalcão.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Cantina
+{
+    partial class telabalcão
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(telabalcão));
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            pictureBox2 = new PictureBox();
+            label2 = new Label();
+            label3 = new Label();
+            pictureBox3 = new PictureBox();
+            lblRelogio = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(3, -25);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(284, 222);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Agrandir Narrow Bold", 48F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(243, 241, 238);
+            label1.Location = new Point(88, 200);
+            label1.Name = "label1";
+            label1.Size = new Size(412, 86);
+            label1.TabIndex = 1;
+            label1.Text = "Cliente Atual";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.FromArgb(243, 241, 238);
+            pictureBox2.Location = new Point(576, 215);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(10, 488);
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Agrandir Narrow Bold", 48F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.FromArgb(243, 241, 238);
+            label2.ImageKey = "(none)";
+            label2.Location = new Point(636, 200);
+            label2.Name = "label2";
+            label2.Size = new Size(396, 86);
+            label2.TabIndex = 3;
+            label2.Text = "Em Preparo:";
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.Bottom;
+            label3.AutoEllipsis = true;
+            label3.Font = new Font("Agrandir Narrow Bold", 48F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.FromArgb(230, 255, 0);
+            label3.Location = new Point(12, 286);
+            label3.Name = "label3";
+            label3.Size = new Size(558, 209);
+            label3.TabIndex = 4;
+            label3.Text = "Exemplo Cliente Atual";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(911, -3);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(543, 732);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 5;
+            pictureBox3.TabStop = false;
+            // 
+            // lblRelogio
+            // 
+            lblRelogio.AutoSize = true;
+            lblRelogio.BackColor = Color.Transparent;
+            lblRelogio.Font = new Font("Agrandir Narrow Bold", 27.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblRelogio.ForeColor = Color.FromArgb(243, 241, 238);
+            lblRelogio.ImageKey = "(none)";
+            lblRelogio.Location = new Point(1114, 64);
+            lblRelogio.Name = "lblRelogio";
+            lblRelogio.Size = new Size(131, 49);
+            lblRelogio.TabIndex = 6;
+            lblRelogio.Text = "00:00";
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick_1;
+            // 
+            // telabalcão
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(17, 25, 12);
+            ClientSize = new Size(1361, 729);
+            Controls.Add(lblRelogio);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(pictureBox2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Controls.Add(pictureBox3);
+            Name = "telabalcão";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "telabalcão";
+            WindowState = FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private PictureBox pictureBox2;
+        private Label label2;
+        private Label label3;
+        private PictureBox pictureBox3;
+        private Label lblRelogio;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ telabalcão.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cantina
+{
+    public partial class telabalcão : Form
+    {
+        public telabalcão()
+        {
+            InitializeComponent();
+        }
+       
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            lblRelogio.Text = DateTime.Now.ToString("HH:mm");
+        }
+    }
+}
